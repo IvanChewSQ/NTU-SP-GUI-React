d1d28225792fe589c01ca1e05425509eaad5c46a
IvanChewSQ <2001303@sit.singaporetech.edu.sg>
2022-10-03 23:13:15 +0800

    Structure of React components and elements

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export { default as Bar } from './Charts/BarChart';
+export { default as LineChart } from './Charts/LineChart';
+export { default as Pie } from './Charts/PieChart';
+export { default as Button } from './Button';
+export { default as ChartsHeader } from './ChartsHeader';
+export { default as Header } from './Header';
+export { default as Sidebar } from './Sidebar';
